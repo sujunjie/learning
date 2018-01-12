3bc902e5d0d9f38e2034128b442b42896766d2ad
sujunjie <sujunjie@sujunjie.cn>
2018-01-12 11:47:29 +0800

    1688

--- javascript/1688.js
+++ javascript/1688.js
@@ -3,29 +3,44 @@
  * @Date：2017/9/7
  */
 
-// 设定输出编码
-//phantom.outputEncoding = "gbk";
-// 清空cookie
-//phantom.clearCookies();
-
 var casper = require('casper').create({
-    clientScripts: ["jquery.min.js"],
+    waitTimeout: 3000,
+    timeout: 80000,
+    exitOnError: false,
     pageSettings: {
         javascriptEnabled: true,
         XSSAuditingEnabled: true,
-        loadImages: true, // 这里设置为false以后图片请求直接忽略，不会进入onResourceRequested
+        loadImages: false, // 这里设置为false以后图片请求直接忽略，不会进入onResourceRequested
         loadPlugins: true,
+        resourceTimeout: 20000,
         userAgent: "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36"
+    },
+    onTimeout: function() {
+        this.echo('[TIME OUT!]');
     },
     onError: function(casper, m) {
         this.echo('[Error:] ' + m);
     },
     onLoadError: function(casper, url, stat) {
         this.echo('[LoadError:] ' + url + '|' + stat);
+        if (url.indexOf('login.taobao.com/jump') > 0
+            || url.indexOf('corp.1688.com/page/index.htm') > 0
+        ) {
+            casper.open(url);
+        }
     },
     onResourceRequested: function(casper, requestData, networkRequest) {
+        //this.echo('Request: ' + requestData.url);
+        if (/.css/.test(requestData.url)) {
+            networkRequest.abort();
+            return true;
+        } 
+    },
+    onResourceReceived: function(casper, resource) {
+        //require('utils').dump(resource);
     }
 });
+
 
 var query = casper.cli.raw.get(0);
 
@@ -33,56 +48,51 @@ var query = casper.cli.raw.get(0);
 casper.start('https://s.1688.com/', function() {
     // 点选供应商tab，此操作会改变表单action
     this.clickLabel('供应商', 'a');
-    // 等待一下，让表单action变更完，不然会跳转到登录页
-    this.wait(2000, function(){
-        // 填写表单并提交
-        this.fillSelectors(
-            'form#j_search_form',
-            {
-                'input#j_q': query
-            },
-            true // submit
-        );
-        // 等待搜索结果页打开，并切换到结果页
-        waitForNewPage.call(this, 0, searchResultPage);
-    });
-});;
-casper.run();
-
-/**
- * 等待一个新窗口打开并切换到新窗口
- */
-function waitForNewPage(index, callback) {
-    this.waitForPopup(
-        index, // 窗口信息，可以是整数，表示第几个打开的窗口，从0开始计数
-        function() {
-            // 切换到新开窗口
-            this.withPopup(
-                index,
-                function() {
-                    // 回调
-                    callback.call(this);
-                }
+    // 等待表单action变更，不然会跳转到登录页
+    this.waitFor(
+        function check() {
+            var formAction = this.getElementInfo('form#j_search_form').attributes.action;   
+            return formAction == '//s.1688.com/company/company_search.htm';
+        }, 
+        function then() {
+            // 尽量在当前页打开新页面，waitForPopup感觉有坑
+            this.evaluate(function() {
+                document.querySelector('#j_search_form').target = '_self';
+            });
+            // 填写表单并提交
+            this.fillSelectors(
+                'form#j_search_form',
+                {
+                    'input#j_q': query
+                },
+                true // submit
+            );
+            // 等待搜索结果页打开
+            this.waitForUrl(
+                /company_search\.htm/, 
+                function then() {
+                    searchResultPage.call(this);
+                },
+                function timeout() {
+                    this.echo('[表单提交超时！]');
+                    this.exit(1);
+                },
+                3000
             );
         },
-        function() {
-            this.echo('[waitForPopup timeout:] waitForPopup超时啦！！！'); 
+        function timeout() {
+            this.echo('[等待表单action变更超时！]');
             this.exit(1);
         },
-        10000
+        3000
     );
-}
+});;
+casper.run();
 
 /**
  * 搜索结果页处理
  */
 function searchResultPage() {
-    // 结果页打开失败
-    if (this.page.url.indexOf('s.1688.com/company/company_search.htm') < 0) {
-        this.echo('[结果页打开失败:] ' + this.page.url); 
-        return this.exit(1);
-    }
-
     // 匹配到的结果条数
     var count = this.getElementInfo('#breadCrumbText .sm-navigatebar-count').text;
     if (0 == count) {
@@ -91,27 +101,57 @@ function searchResultPage() {
     }
     
     // 第一条结果标题是公司名称，需要跟query精确匹配
-    var result1_title = this.getElementInfo('#offer1 a[offer-stat="com"]').attributes.title;
-    //require('utils').dump(result1_title);
-    if (result1_title != query) {
-        this.echo('[结果未能精确匹配:]' + result1_title);
+    var resultTitle = this.getElementInfo('#offer1 a[offer-stat="com"]').attributes.title;
+    if (resultTitle != query) {
+        this.echo('[结果未能精确匹配:]' + query + ' != ' + resultTitle);
         return this.exit(2);
     }
 
     // 点击“更多公司信息”跳转到诚信档案页
-    this.wait(2000, function(){
-        this.clickLabel('更多公司信息>', 'a');
-        // 等待诚信档案页打开，并切换到诚信档案页
-        waitForNewPage.call(this, 0, creditDetailPage);
+    this.evaluate(function() {
+        document.querySelector('#offer1 a[offer-stat="morecompany"]').target = '_self';
     });
-    // 截图
-    // this.capture('1688_r.png');
+    this.clickLabel('更多公司信息>', 'a');
+    // 等待诚信档案页/黄页打开
+    this.waitForUrl(
+        /creditdetail\.htm|corp\.1688\.com/,
+        function then() {
+            creditDetailPage.call(this);
+        },
+        function timeout() {
+            this.echo('[等待诚信档案页/黄页打开超时！]');
+            this.exit(1);
+        },
+        3000
+    );
 }
 
 /**
- * 诚信档案页处理
+ * 诚信档案页/黄页处理
  */
 function creditDetailPage() {
-
+    this.echo(this.page.url);
     this.capture('1688_c.png');
+    // 如果直接是黄页，返回页面内容
+    if (this.page.url.indexOf('corp.1688.com/page/index.htm') > 0) {
+        this.echo(this.getPageContent());
+        this.exit(0);
+    // 否则查看页面是否有黄页链接，点击
+    } else {
+        this.evaluate(function() {
+            document.querySelector('a#J_COMMON_GoToYellowPage').target = '_self';
+        });
+        this.click('a#J_COMMON_GoToYellowPage'); 
+        this.waitForUrl(
+            /corp\.1688\.com/,
+            function then() {
+                creditDetailPage.call(this);
+            },
+            function timeout() {
+                this.echo('[等待黄页打开超时！]');
+                this.exit(1);
+            },
+            3000
+        );
+    }
 }
