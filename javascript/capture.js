67c8b532879d30bd30a53300682b2eb7f8c3871a
sujunjie <sujunjie@sujunjie.cn>
2018-01-15 10:49:34 +0800

    网页截图

--- javascript/capture.js
+++ javascript/capture.js
@@ -1,6 +1,6 @@
 // 使用casperjs截图web页面
 // 使用方法：
-//     casperjs capture.js 'www.sujunjie.cn'
+//     casperjs capture.js 'http://www.sujunjie.cn'
 var casper = require('casper').create({
     pageSettings: {
         javascriptEnabled: true,
@@ -10,16 +10,21 @@ var casper = require('casper').create({
         userAgent: "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36"
     },
     onError: function(casper, m) {
+        this.echo(m);
     },
     onLoadError: function(casper, url, stat) {
+        this.echo(url);
     }
 });
 
+// 要请求的url
 var url = casper.cli.raw.get(0);
+// 可以指定一个延迟时间，防止一些js setTimeout跳转。单位：毫秒
+var waitMilliSecond = parseInt(casper.cli.raw.get(1), 10);
 casper.start(url);
 
 casper.then(function() {
-    this.wait(5000, function(){
+    this.wait(waitMilliSecond, function(){
         this.echo(this.page.url);
         this.echo(this.getTitle());
         this.capture('c.png');
